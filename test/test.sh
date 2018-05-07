6d22c39fbdca33f7f849a0cebd886d1f8e0e362b
Zhani Baramidze <jbaramidze@gmail.com>
2018-05-07 23:46:52 +0200

    Added one correct testcase handling

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+echo "Testing vulnerable cases"
 for filename in testcases/vuln*.cpp; do
   echo -n "Testing ${filename}.......  "
   executable=${filename::-4}
@@ -13,9 +15,29 @@ for filename in testcases/vuln*.cpp; do
   if [ "$?" -ne "0" ]; then
     echo "FAIL!"
     echo "Command: ${cmd}"
+    exit
   else
     echo "Success."
-  fi 
+  fi
+done
+
+echo "Testing correct cases"
+for filename in testcases/correct*.cpp; do
+  echo -n "Testing ${filename}.......  "
+  executable=${filename::-4}
+  cmd="~/Thesis/installer/dynamorio/build/bin64/drrun -c /home/zhani/Thesis/project/build/bin/libnashromi.so \
+          -- \"/home/zhani/Thesis/project/test/build/${executable}\" 2>&1 | grep --silent \"\!\!\!WARNING\!\!\!\""
 
 
+   ~/Thesis/installer/dynamorio/build/bin64/drrun -c /home/zhani/Thesis/project/build/bin/libnashromi.so \
+          -- "/home/zhani/Thesis/project/test/build/${executable}" 2>&1 | grep --silent "\!\!\!WARNING\!\!\!"
+
+
+  if [ "$?" -ne "1" ]; then
+    echo "FAIL!"
+    echo "Command: ${cmd}"
+    exit
+  else
+    echo "Success."
+  fi
 done
---
+++ test/testcases/correct_basic1.cpp
@@ -0,0 +1,17 @@
+#include "/custobuilds/include/stdio.h"
+#include "/custobuilds/include/stdlib.h"
+#include "dr_annotations_zhani.h"
+
+int A[20];
+
+int main()
+{
+  int a = 15;
+
+  nshrtaint((long long int) &a, 4);
+
+  if (a < 30 && a > 0)
+  {
+    int b = A[a];
+  }
+}
