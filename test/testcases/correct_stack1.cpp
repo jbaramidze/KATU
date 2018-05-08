b3c7c432a42584fab28f4145103bf01c3a0e9f18
Zhani Baramidze <jbaramidze@gmail.com>
2018-05-08 14:37:32 +0200

    Testcases for stack modifications

--- test/testcases/correct_stack1.cpp
+++ test/testcases/correct_stack1.cpp
@@ -2,16 +2,6 @@
 #include "/custobuilds/include/stdlib.h"
 #include "dr_annotations_zhani.h"
 
-const char *path = "/home/zhani/Thesis/test/zaza";
-
-extern "C"
-{
-  void func()
-  {    
-
-  }
-}
-
 int A[20];
 
 int main()
@@ -20,12 +10,8 @@ int main()
   int b = 3;
 
   nshrtaint((long long int) &a, 4);
-  //nshrtaint((long long int) &b, 4);
-
-  //dynamorio_annotate_zhani_signal(1);
- /////////////////////////////////////////////////////////////////
-
-     asm volatile (	
+     
+  asm volatile (	
    	"pushq $10 \n \t"
    	"push $10 \n \t"
    	"push %%rax \n \t"
@@ -41,11 +27,4 @@ int main()
   {
   	int h = A[b];
   }
-  
-
-/////////////////////////////////////////////////////////////////
-  //dynamorio_annotate_zhani_signal(0);
-
-/*  printf("Opened: %d.\n", a);
-  printf("Read %d bytes.\n", rd);*/
 }
---
+++ test/testcases/vuln_stack1.cpp
@@ -0,0 +1,30 @@
+#include "/custobuilds/include/stdio.h"
+#include "/custobuilds/include/stdlib.h"
+#include "dr_annotations_zhani.h"
+
+int A[20];
+
+int main()
+{
+  int a = 15;
+  int b = 3;
+
+  nshrtaint((long long int) &a, 4);
+     
+  asm volatile (	
+   	"pushq $10 \n \t"
+   	"push $10 \n \t"
+   	"push %%rax \n \t"
+   	"push $10 \n \t"
+   	"pop %%rax \n \t"
+   	"pop %%rbx \n \t"
+   	"pop %%rax \n \t"
+   	"popq %%rax \n \t"
+   	:"=b"(b) : "a"(a) : 
+   	);
+
+  if (a > 10)
+  {
+  	int h = A[b];
+  }
+}
