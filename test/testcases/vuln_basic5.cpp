e694419052c8cb8af7e0501c06ad7f0294e91164
Zhani Baramidze <jbaramidze@gmail.com>
2018-05-15 20:21:46 +0300

    Added testcase

---
+++ test/testcases/vuln_basic5.cpp
@@ -0,0 +1,23 @@
+#include "/custobuilds/include/stdio.h"
+#include "/custobuilds/include/stdlib.h"
+#include "dr_annotations_zhani.h"
+
+const char *path = "/home/zhani/Thesis/test/zaza";
+
+
+void (*p[4]) (int x, int y);
+
+int main()
+{
+
+  int a = 1;
+  int b = 3;
+  int c = 3;
+
+  nshrtaint((long long int) &a, 4);
+  nshrtaint((long long int) &b, 4);
+  nshrtaint((long long int) &c, 4);
+
+  (*p[a]) (2, 3);
+  
+}
